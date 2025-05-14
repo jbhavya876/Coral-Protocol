66f6d42031a2a720eaa3bf8340fb522c03f0a49d
Caelum Forder <caelum119@gmail.com>
2025-05-14 15:47:57 +0100

    Cleanup

--- src/test/kotlin/org/coralprotocol/coralserver/e2e/E2EResourceTest.kt
+++ src/test/kotlin/org/coralprotocol/coralserver/e2e/E2EResourceTest.kt
@@ -16,57 +16,28 @@ import kotlin.test.Test
 class E2EResourceTest {
     val port = 14391
     var server = CoralServer(port = port, devmode = true)
-    val context = newFixedThreadPoolContext(1, "E2EResourceTest")
+
+    @OptIn(DelicateCoroutinesApi::class)
+    val serverContext = newFixedThreadPoolContext(1, "E2EResourceTest")
 
     @OptIn(DelicateCoroutinesApi::class)
     @BeforeEach
     fun setup() {
         server.stop()
         server = CoralServer(port = port, sessionManager = SessionManager(), devmode = true)
-        GlobalScope.launch(context) {
+        GlobalScope.launch(serverContext) {
             server.start()
         }
-        mockkStatic(io.modelcontextprotocol.util.Utils::class)
-        mockkStatic(HttpRequest::class)
-        every { HttpRequest.newBuilder() } answers {
-            println("MockK Interceptor [@BeforeEach]: HttpRequest.newBuilder() called. ")
-            val requestBuilder = callOriginal().headers("Content-Type", "application/json")
-            return@answers requestBuilder
-        }
-
-
-        every { resolveUri(any<URI>(), any<String>()) } answers {
-            val actualBaseUrl = invocation.args[0] as URI
-            val actualEndpointUrl = invocation.args[1] as String
-            print("MockK Interceptor [@BeforeEach]: Utils.resolveUri called with baseUrl='$actualBaseUrl', endpointUrl='$actualEndpointUrl'. ")
-
-            println("Using original endpoint '$actualBaseUrl'")
-
-            return@answers if (actualEndpointUrl.contains("?sessionId")) {
-                actualBaseUrl.resolve(actualEndpointUrl)
-            } else {
-                actualBaseUrl
-            }
-        }
+        patchMcpJavaContentType()
+        patchMcpJavaContentType()
     }
 
 
     @Test
     fun testE2EResource() {
-        // Create a connected Coral agent using the utility
         val agent1 = createConnectedCoralAgent(server, "testAgent", "testAgentDescription", "testSystemPrompt")
 
-        // Test the agent
         runBlocking {
-//            delay(100000)
-//            agentgetAgents().forEach { agent ->
-//                val chatAgent = agent as ChatAgent
-//                println("Agent name: ${chatAgent.name}")
-//                val resp = chatAgent.ask("Create a thread and post a message in it")
-//                val sessions = SessionManager.getAllSessions()
-//                assertEquals(1, sessions.size, "There should be one session")
-//                println("Response: $resp")
-//            }
             val response = agent1.ask("Create a thread and post a message in it")
             println("Agent name: ${agent1.name}")
             val sessions = server.sessionManager.getAllSessions()
@@ -76,5 +47,30 @@ class E2EResourceTest {
     }
 }
 
+
+private fun patchMcpJavaContentType() {
+    mockkStatic(HttpRequest::class)
+    every { HttpRequest.newBuilder() } answers {
+        println("MockK Interceptor [@BeforeEach]: HttpRequest.newBuilder() called. ")
+        val requestBuilder = callOriginal().headers("Content-Type", "application/json")
+        return@answers requestBuilder
+    }
+}
+
+private fun patchMcpJavaEndpointResolution() {
+    mockkStatic(io.modelcontextprotocol.util.Utils::class)
+    every { resolveUri(any<URI>(), any<String>()) } answers {
+        val baseUrl = invocation.args[0] as URI
+        val endpointUrl = invocation.args[1] as String
+        print("MockK Interceptor [@BeforeEach]: Utils.resolveUri called with baseUrl='$baseUrl', endpointUrl='$endpointUrl'. ")
+        return@answers if (endpointUrl.contains("?sessionId")) {
+            // In this case the sessionId is an MCP sessionId, not a Coral sessionId.
+            // The resolution logic works in this case (though the original is resolving against a URI object)
+            baseUrl.resolve(endpointUrl)
+        } else {
+            baseUrl
+        }
+    }
+}
 
 
