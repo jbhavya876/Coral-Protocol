9aca06728b14ebf1e33c8e41b417150e2401eb30
Alan Panayotov <alanpanayotov@gmail.com>
2025-05-21 16:38:07 +0100

    feat: ResolvedThread

--- src/main/kotlin/org/coralprotocol/coralserver/models/Thread.kt
+++ src/main/kotlin/org/coralprotocol/coralserver/models/Thread.kt
@@ -16,3 +16,24 @@ data class Thread(
     var isClosed: Boolean = false,
     var summary: String? = null
 )
+
+@Serializable
+data class ResolvedThread(
+    val id: String = UUID.randomUUID().toString(),
+    val name: String,
+    val creatorId: String,
+    val participants: List<String> = listOf(),
+    val messages: List<ResolvedMessage> = listOf(),
+    var isClosed: Boolean = false,
+    var summary: String? = null
+)
+
+fun Thread.resolve(): ResolvedThread = ResolvedThread(
+    id = id,
+    name = name,
+    creatorId = creatorId,
+    participants = participants,
+    messages = messages.map { it.resolve() },
+    isClosed = isClosed,
+    summary = summary
+)
